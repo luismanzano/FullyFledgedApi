46901c99626e3d930cf1e110d4b8876f3fa9b577
Luis Fernando Manzano <luis.m@rivka.mx>
2022-07-07 20:42:04 -0400

    dded the first post request
    now that i'm learning how to develop api's
    I have tested the paths with postman and all seem to work
    correctly, added a post request too! Will test

--- main.py
+++ main.py
@@ -4,4 +4,12 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "HI!! THIS IS MY FIRST API WITH FASTAPI"}
+
+@app.get("/posts")
+async def root():
+    return {"post": "Congratulations on your first Post!!"}
+
+@app.post("/createpost")
+async def root():
+    return {"data": "Post Succesfully Created"}
